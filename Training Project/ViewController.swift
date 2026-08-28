c031ee3d7abfc7af6288210c46c08745ce5c6c17
Akar Jaza <akar.jaza1212@gmail.com>
2026-08-28 21:14:50 +0300

    Initial Commit

---
+++ Training Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training Project
+//
+//  Created by Akar jaza on 8/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
