9f2f62994178484941f5511655b3bf75f828501d
Akar Jaza <akar.jaza1212@gmail.com>
2026-09-15 18:05:26 +0300

    refactor: migrate ProductFormViewModel and ProductsViewModel to use RxSwift for network requests

--- Training Project/Features/Products/ViewModel/ProductFormViewModel.swift
+++ Training Project/Features/Products/ViewModel/ProductFormViewModel.swift
@@ -6,102 +6,65 @@
 //
 
 import Foundation
+import RxSwift
 
 class ProductFormViewModel {
     var onSuccess: ((Product) -> Void)?
     var onError: ((Error) -> Void)?
-
+    var networkService: NetworkServiceProtocol = NetworkService.shared
+    var disposeBag = DisposeBag()
+    
     func createProduct(title: String, description: String, price: Double) {
         guard let url = URL(string: "https://dummyjson.com/products/add") else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
         let body: [String: Any] = [
             "title": title,
             "description": description,
             "price": price
         ]
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: body)
-        } catch {
-            onError?(error)
-            return
-        }
-        
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            
-            if let error = error {
-                DispatchQueue.main.async { self?.onError?(error) }
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            do {
-                let decoded = try JSONDecoder().decode(CreateProductResponse.self, from: data)
-                
+        networkService.request(url: url, method: .post, body: body)
+            .subscribe(onNext: { [weak self] (response: CreateProductResponse) in
                 let newProduct = Product(
-                    id: decoded.id,
+                    id: response.id,
                     title: title,
                     description: description,
                     price: price,
                     thumbnail: ""
                 )
-                
                 DispatchQueue.main.async {
                     self?.onSuccess?(newProduct)
                 }
-            } catch {
+            }, onError: { [weak self] error in
                 DispatchQueue.main.async { self?.onError?(error) }
-            }
-            
-        }.resume()
-        
-       
+            })
+            .disposed(by: disposeBag)
     }
     
     func updateProduct(id: Int, title: String, description: String, price: Double, currentThumbnail: String) {
         guard let url = URL(string: "https://dummyjson.com/products/\(id)") else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "PUT"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
         let body: [String: Any] = [
             "title": title,
             "description": description,
             "price": price
         ]
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: body)
-        } catch {
-            onError?(error)
-            return
-        }
-        
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            if let error = error {
+        networkService.request(url: url, method: .put, body: body)
+            .subscribe(onNext: { [weak self] (response: CreateProductResponse) in
+                let updatedProduct = Product(
+                    id: id,
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: currentThumbnail,
+                )
+                
+                DispatchQueue.main.async {
+                    self?.onSuccess?(updatedProduct)
+                }
+            }, onError: { [weak self] error in
                 DispatchQueue.main.async { self?.onError?(error) }
-                return
-            }
-            
-            let updatedProduct = Product(
-                id: id,
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: currentThumbnail,
-            )
-            
-            DispatchQueue.main.async {
-                self?.onSuccess?(updatedProduct)
-            }
-        }.resume()
+            })
+            .disposed(by: disposeBag)
     }
-    
-    
 }
+
+
+
--- Training Project/Features/Products/ViewModel/ProductsViewModel.swift
+++ Training Project/Features/Products/ViewModel/ProductsViewModel.swift
@@ -6,64 +6,48 @@
 //
 
 import Foundation
-
+import RxSwift
 
 class ProductsViewModel {
     private(set) var products: [Product] = []
+    var networkService: NetworkServiceProtocol = NetworkService.shared
     
     var onProductsUpdated: (() -> Void)?
     var onError: ((Error) -> Void)?
+    var disposeBag = DisposeBag()
     
     func fetchProducts() {
         guard let url = URL(string: "https://dummyjson.com/products") else { return }
         
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            if let error = error {
+        networkService.request(url: url, method: .get, body: nil)
+            .subscribe(onNext: { [weak self] (response: ProductsResponse) in
+                self?.products = response.products
+                self?.onProductsUpdated?()
+            }, onError: { [weak self] error in
                 self?.onError?(error)
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            do {
-                let decoded = try JSONDecoder().decode(ProductsResponse.self, from: data)
-                self?.products = decoded.products
-                DispatchQueue.main.async {
-                    self?.onProductsUpdated?()
-                }
-            } catch {
-                self?.onError?(error)
-            }
-        }.resume()
+            })
+            .disposed(by: disposeBag)
     }
     
     func deleteProduct(_ product: Product, at index: Int) {
         guard let url = URL(string: "https://dummyjson.com/products/\(product.id)") else { return }
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "DELETE"
-        
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            
-            if let error = error {
-                DispatchQueue.main.async { self?.onError?(error) }
-                return
-            }
-            
-            // Dummy json won't delete it on their server because DummyJson server doesn't exist, we remove it locally either way. THis func is just for learning purposes
+        networkService.requestData(url: url, method: .delete).subscribe(onNext: { [weak self] data in
             DispatchQueue.main.async {
                 guard self?.products.indices.contains(index) == true else { return }
                 self?.products.remove(at: index)
                 self?.onProductsUpdated?()
             }
-            
-        }.resume()
+        }, onError: { [weak self] error in
+            DispatchQueue.main.async {
+                self?.onError?(error)
+            }
+        }).disposed(by: disposeBag)
     }
-
-    
+        
     func addProduct(_ product: Product) {
         products.insert(product, at: 0)
-        onProductsUpdated?() 
+        onProductsUpdated?()
     }
     
     func replaceProduct(_ product: Product, at index: Int) {
