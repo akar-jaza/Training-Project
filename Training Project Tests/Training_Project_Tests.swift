7c0b041addcd11a603ef2069d2ddd4b82a39d45c
Akar Jaza <akar.jaza1212@gmail.com>
2026-09-15 22:06:01 +0300

    feat: add unit testing target and configurations for Training Project

---
+++ Training Project Tests/Training_Project_Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Training_Project_Tests.swift
+//  Training Project Tests
+//
+//  Created by Akar jaza on 9/15/26.
+//
+
+import Testing
+
+struct Training_Project_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
